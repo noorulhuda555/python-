189a87c2227ccfe8787725e1bdffb2ef37a108d1
noor ul huda <noorulhuda2114@gmail.com>
2024-07-21 09:16:15 +0500

    Add files via upload

---
+++ 4_variables.py
@@ -0,0 +1,26 @@
+# Unlike other programming languages, Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+# cannot make varible until u assin value to it using = operator 
+# first letter = a to z or _ 
+# after first u may use numbers , _ , a - z
+# small and caps different  + key word ot allowed + no spaces+ no special characters 
+
+integer = 5
+ch = 'x'
+string = 'noor'
+float = 2.3
+double = 2.4488
+string2 = "string2"
+
+print('integer type ', integer)
+print('character', ch)
+print('string', string)
+print('double', double)
+print('string', string2)
+
+#printing separted by comma 
+print('the val is', 'number') 
+print('the val is ', integer)
+
+# 72 = amount  //error invalid assignment 
+
---
+++ 5_operators.py
@@ -0,0 +1,38 @@
+n1 = 2
+n2 = 4
+n3 = 2
+#listed in order of highest to lowest precedence
+print(n1 ** n2) #power right to left assocition
+#all below have left ro right association
+print(n1 * n2) #multiply 
+print(n1 / n2 ) #divides and give float ans
+print(n1 // n2) # division asnwer as int
+print(n1 % n2) #remainder
+print(n1 + n2) #add
+print(n1 - n2) #subtract
+
+#logical operators 
+if n1 < n2 :
+    print('n1 is less than n2')
+
+if n2 > n1 :
+    print('n2 is greater than n1')
+
+if n1 == n3 :
+    print('n1 and n3 are equal')
+
+if n1 >= n3 :
+    print('n1 is greater or equal to n1')
+
+if n1 != n2 :
+    print('n1 is not equal to n2')
+
+if n1 == n3 : 
+    print('n1 and n3 are equal')
+
+
+
+
+
+
+
