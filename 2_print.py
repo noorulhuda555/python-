ca7b7d93879e45d1e96b78e04ed2c14676ae82f6
noor ul huda <noorulhuda2114@gmail.com>
2024-07-21 09:19:02 +0500

    Update 2_print.py

--- 2_print.py
+++ 2_print.py
@@ -5,18 +5,23 @@ print("""I'm reading "Hamlet" tonight.""") #triple quotes
 print("""One 
 Two
 Three""") #multiple line
+
 print(
-    'one'
-) #with single quote 
+    'one'    #with single quote 
+) 
+
 #exerise questions
 print("python's the best!") 
 print('the cat said "meow".')
+
 #printing in a single line 
 print('one ', end='')
 print('two' , end='')
 
+#printing in two lines 
 print('\nmove'
       )
+
 print('first\nsecond\nthird') #new line
 print('mon\ttues\twed') #skip tab position
 print('i m ready \'') #print a single quote
@@ -35,21 +40,25 @@ print(n)
 print(format(n, '.2f'))
 print(format(n, '.1f'))
 print(format(n, '.3f'))
+
 #print numbers in scientific notation
 print(format(n, 'e'))
 print(format(n, '.2e'))
+
 #print using comma separator = thousands place 
 print(format(n, ',.2f')) 
 print(format(n, ','))
-#set precison
+
+#set precision
 print('The number is', format(12345.6789, '12,.2f')) 
 print('The number is', format(12345.6789, '12,')) 
+
 #percentage 
 print(format(0.5, '%')) #multplies number with 100 and places % sign 
-print(format(0.5, '.0%')) #0 precison
+print(format(0.5, '.0%')) #0 precision
 
 #formatting ints
-#annot tell preciosn as no comma and use d instead of f as type designator 
+#annot tell precision as no comma and used instead of f as type designator 
 print(format(12345, ',d')) #comma place
 print(format(12345, '10d')) #move 10spaces , no comma
 print(format(12345, '10,d')) #comma + move
